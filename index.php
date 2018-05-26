5da2687cb6455b9f26d62e6cb9595d143df410f6
shimantoneer <poddernakla@gmail.com>
2018-05-26 23:33:36 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: http://hubblesite.org/hubble_discoveries/dark_energy/de-did_einstein_predict.php");
 die();
 ?>
